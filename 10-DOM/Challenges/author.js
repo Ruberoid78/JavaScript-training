34d50caff9de82d440737ec3ff15c6c0060322af
Ruberoid78 <83211397+Ruberoid78@users.noreply.github.com>
2022-11-12 16:34:40 +0300

    4th commit

--- 10-DOM/Challenges/author.js
+++ 10-DOM/Challenges/author.js
@@ -1,24 +1,20 @@
 const list = document.createElement('ul');
 document.body.appendChild(list);
 
-function add(item){
-  const li = document.createElement('li');
-  li.innerHTML = item;
-  list.appendChild(li);
+function add(item) {
+    const li = document.createElement('li');
+    li.innerHTML = item;
+    list.appendChild(li);
 }
 
-function toggle(item){
-  item.classList.toggle('complete');
+function toggle(item) {
+    item.classList.toggle('complete');
 }
 
-function destroy(item){
-  item.remove();
+function destroy() {
+    list.lastChild.remove();
 }
-
 
 add('Shoes');
 add('Trainers');
 add('Jacket');
-
-
-destroy('Jacket');
