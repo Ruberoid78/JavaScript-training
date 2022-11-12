480f97124f71ce03ad1df25f25858125b4d3ba34
Ruberoid78 <83211397+Ruberoid78@users.noreply.github.com>
2022-11-12 20:57:27 +0300

    11-MainEvents First Update

---
+++ 11-mainEvents/functions.js
@@ -0,0 +1,4 @@
+export function bang() {
+    document.body.style.background = "yellow";
+    document.body.innerHTML = `<h1> BANG!!!</h1>`;
+}
---
+++ 11-mainEvents/index.js
@@ -0,0 +1,3 @@
+import { bang } from "./functions.js";
+
+document.addEventListener("click", bang);
