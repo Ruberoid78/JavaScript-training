066290ee27b8afce279b11b4a807574af1de8904
Ruberoid78 <83211397+Ruberoid78@users.noreply.github.com>
2022-11-16 19:23:27 +0300

    Last update for 11 - Main Events

--- 11-mainEvents/Practice06/index.js
+++ 11-mainEvents/Practice06/index.js
@@ -8,4 +8,19 @@ function addTask(e) {
     const task = document.createElement("li");
     task.textContent = form.newTask.value;
     list.appendChild(task);
+    form.newTask.value = "";
+    form.newTask.focus();
+}
+
+list.addEventListener("click", strikeTask);
+
+/* function strikeTask(e) {
+    //alert(e.target.textContent);
+    e.target.classList.toggle("complete");
+}
+ */
+
+function strikeTask(e) {
+    e.target.remove();
+    //  e.target.parentNode.removeChild(e.target);
 }
